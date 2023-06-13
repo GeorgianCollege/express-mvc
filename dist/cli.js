7a268fe6e071f71f8b841d65e96e9b84b09c543d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-06-13 18:19:24 -0400

    updated cli

--- dist/cli.js
+++ dist/cli.js
@@ -4,20 +4,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
 const generator_1 = require("./generator");
 const program = new commander_1.Command();
-program.version('1.0.9').description('Generate an MVC project using Node and Express');
 program
-    .argument('[folder]', 'Project folder path')
-    .option('-h, --help', 'Display help')
+    .version('1.0.10')
+    .description('Generate an MVC project using Node and Express')
     .option('--tsc', 'Generate with TypeScript')
+    .option('-h, --help', 'Display help')
+    .argument('[folder]', 'Project folder path')
     .parse(process.argv);
-if (program.help) {
+const options = program.opts();
+if (options.help) {
     program.help();
 }
 else {
     const projectFolder = program.args[0] || process.cwd();
     console.log("generating project");
     console.log("------------------");
-    const options = program.opts();
     const useTypeScript = (options.tsc === true);
     console.log((useTypeScript) ? "--with TypeScript" : "--no TypeScript");
     (0, generator_1.generate)((projectFolder == program.args[0]) ? projectFolder : "", useTypeScript);
--- src/cli.ts
+++ src/cli.ts
@@ -4,15 +4,18 @@ import { Command } from 'commander';
 import { generate } from './generator';
 
 const program = new Command();
-program.version('1.0.9').description('Generate an MVC project using Node and Express');
 
 program
-  .argument('[folder]', 'Project folder path')
-  .option('-h, --help', 'Display help')
+  .version('1.0.10')
+  .description('Generate an MVC project using Node and Express')
   .option('--tsc', 'Generate with TypeScript')
+  .option('-h, --help', 'Display help')
+  .argument('[folder]', 'Project folder path')
   .parse(process.argv);
 
-  if (program.help) 
+  const options = program.opts();
+
+  if (options.help) 
   {
     program.help();
   } 
@@ -23,7 +26,7 @@ program
     console.log("generating project");
     console.log("------------------");
 
-    const options = program.opts();
+    
     const useTypeScript = (options.tsc === true);
     
     console.log((useTypeScript) ? "--with TypeScript" : "--no TypeScript");
