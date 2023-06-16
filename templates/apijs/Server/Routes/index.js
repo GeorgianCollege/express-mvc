99e7ed43fba2dbbd0c1258998228598f7ba85abd
ifotn <rich@infrontofthenet.com>
2023-06-16 09:19:46 -0400

    refactor routes w/REST patterns

--- templates/apijs/Server/Routes/index.js
+++ templates/apijs/Server/Routes/index.js
@@ -4,32 +4,32 @@ let router = express.Router();
 /* Get the movie Controller */
 const MovieController = require('../Controllers/movie');
 
-/* GET /api/list  - display the movie list */
-router.get('/list', function(req, res, next)
+/* GET /api/movies  - display the movie list */
+router.get('/', (req, res, next) => 
 {
   MovieController.DisplayMovieList(req, res, next);
 });
 
-/* GET /api/find/:id - display a movie by id */
-router.get('/find/:id', function(req, res, next)
+/* GET /api/movies/:id - display a movie by id */
+router.get('/:id', (req, res, next) => 
 {
   MovieController.DisplayMovieByID(req, res, next);
 });
 
-/* POST /api/add - add a new movie */
-router.post('/add', function(req, res, next)
+/* POST /api/movies - add a new movie */
+router.post('/', (req, res, next) => 
 {
   MovieController.AddMovie(req, res, next);
 });
 
-/* POST /api/update/:id - update a movie by id */
-router.put('/update/:id', function(req, res, next)
+/* PUT /api/movies/:id - update a movie by id */
+router.put('/:id', (req, res, next) => 
 {
   MovieController.UpdateMovie(req, res, next);
 });
 
-/* GET /api/delete/:id - delete a movie by id */
-router.delete('/delete/:id', function(req, res, next)
+/* DELETE /api/movies/:id - delete a movie by id */
+router.delete('/:id', (req, res, next) =>
 {
   MovieController.DeleteMovie(req, res, next);
 });
